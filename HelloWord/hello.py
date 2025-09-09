5225c5d018a91dd05536f541013c0e418b8de6bd
HugoG-S <231320695+HugoG-S@users.noreply.github.com>
2025-09-10 01:05:07 +0200

    Day 0

---
+++ HelloWord/hello.py
@@ -0,0 +1,12 @@
+# Esto es un comentario:
+# Nuestro hola mundo en Python
+
+"""
+Este es un 
+comentario 
+en varias lineas
+"""
+
+print("Hola Python")
+
+print('Hola Python') # Declaramos una cadena de texto con comilla simple
---
+++ TipoDatos/datos.py
@@ -0,0 +1,10 @@
+
+
+
+# Con Type vemos el tipo de dato
+
+print(type("Hola Python")) # Tipo 'str'
+print(type(5)) # Tipo 'int'
+print(type(1.5)) # Tipo 'float'
+print(type(1 + 2j)) # Tipo 'complex' 
+print(type(True)) # Tipo 'bool'
